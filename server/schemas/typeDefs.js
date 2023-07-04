c5e0c0b504a5611859568fd368754054de9dffb8
Josh Sandquist <joshdsand@gmail.com>
2023-07-03 18:36:27 -0700

    updated typedefs

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,24 +1,44 @@
 const { gql } = require('apollo-server-express');
 
 const typeDefs = gql`
-type User {
-    id: Int!
-}
+  type User {
+    _id: ID
+    username: String
+    email: String
+    profile: Profile
+  }
 
-type Auth {
+  type Profile {
+    _id: ID
+    bio: String
+    games: [Game]
+  }
+
+  type Game {
+    title: String
+    competitive: Boolean
+    rank: String
+    console: String
+    gamingUsername: String
+  }
+
+  type Auth {
     token: ID
     user: User
-}
+  }
 
-type Query {
-    users: [Users]
-    user: User
-    profiles( user: ID, name: String): [Profile]
-}
+  type Query {
+    users: [User]
+    user(userId: ID!): User
+    profiles(userId: ID, name: String): [Profile]
+  }
 
-type Mutation {
+  type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+    addProfile(userId: ID!, bio: String, games: [GameInput]): User
+  }
+  `;
 
-}
-`;
 
 module.exports = typeDefs
--- server/utils/auth.js
+++ server/utils/auth.js
@@ -1,6 +1,6 @@
 const jwt = require('jsonwebtoken');
 
-const secret = 'mysecretssshhhhhhh';
+const secret = 'secretcode123';
 const expiration = '2h';
 
 module.exports = {
